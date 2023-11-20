83ca6729a5a820718cf921e4c10eda3c13e08802
Ana paula <149811410+anapaulasanto@users.noreply.github.com>
2023-11-19 21:55:35 -0300

    remove js

--- main.js
+++
@@ -1,6 +0,0 @@
-var iconBar = document.querySelector('.menu-icon');
-var menuBar = document.querySelector('.text-bar');
-
-iconBar.addEventListener('click',function(){
-    menuBar.classList.toggle('hide');
-});
